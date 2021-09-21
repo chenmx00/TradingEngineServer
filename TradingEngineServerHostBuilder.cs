2590e130a74f8c2b76f85e0411901485c0e4e84f
chenmx00 <chenmx00@berkeley.edu>
2021-09-21 19:17:38 -0400

    sec

--- TradingEngineServerHostBuilder.cs
+++ TradingEngineServerHostBuilder.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.Extensions.Hosting;
-using Polly;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TradingEngineServer.Core.Configuration;
 
 namespace TradingEngineServer.Core
 {
     public sealed class TradingEngineServerHostBuilder
     {  
-        public static  IHost BuildTradingEngineServer() => Host.CreateDefaultBuilder().ConfigureServices((Context, services)) => {
+        public static  IHost BuildTradingEngineServer() => Host.CreateDefaultBuilder().ConfigureServices((context, services) => 
+        {
+            //Start with Configuration
+            services.AddOptions();
+            services.Configure<TradingEngineServerConfiguration>(context.Configuration.GetSection(nameof(TradingEngineServerConfiguration)))
         }).Build();
     }
 }
